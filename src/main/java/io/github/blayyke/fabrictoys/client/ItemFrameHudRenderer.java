7e7ab894c75185f21794e867c3dd94bff08f078f
Blake Redenius <blakeredenius@gmail.com>
2019-06-21 19:55:14 +1000

    Add config option to prevent drawing item frame info.

--- src/main/java/io/github/blayyke/fabrictoys/client/ItemFrameHudRenderer.java
+++ src/main/java/io/github/blayyke/fabrictoys/client/ItemFrameHudRenderer.java
@@ -1,5 +1,6 @@
 package io.github.blayyke.fabrictoys.client;
 
+import io.github.blayyke.fabrictoys.FabricToys;
 import io.github.blayyke.fabrictoys.accessor.PlayerLookStackAccessor;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.hud.InGameHud;
@@ -14,18 +15,20 @@ import java.util.List;
 public class ItemFrameHudRenderer implements HudRenderer {
     @Override
     public void draw(float f, InGameHud hud, PlayerEntity player) {
-        PlayerLookStackAccessor stackAccessor = (PlayerLookStackAccessor) player;
-        ItemStack stack = stackAccessor.getLookStack();
-        if (stack == null || stack.isEmpty()) {
-            return;
-        }
-        TextRenderer font = hud.getFontRenderer();
-        List<Component> tooltipText = stack.getTooltipText(player, TooltipContext.Default.ADVANCED);
+        if (FabricToys.CONFIG.enableItemFrameInfo) {
+            PlayerLookStackAccessor stackAccessor = (PlayerLookStackAccessor) player;
+            ItemStack stack = stackAccessor.getLookStack();
+            if (stack == null || stack.isEmpty()) {
+                return;
+            }
+            TextRenderer font = hud.getFontRenderer();
+            List<Component> tooltipText = stack.getTooltipText(player, TooltipContext.Default.ADVANCED);
 //        System.out.println("Drawing text : " + tooltipText.size());
-        font.drawWithShadow(stack.getDisplayName().getText(), 50f, 50f - font.fontHeight - 1, 0xFFFFFFFF);
-        for (int i = 0; i < tooltipText.size(); i++) {
-            Component component = tooltipText.get(i);
-            font.drawWithShadow(new TranslatableComponent(component.getText()).getText(), 50f, 50f + (i * font.fontHeight + 1), 0xFFFFFFFF);
+            font.drawWithShadow(stack.getDisplayName().getText(), 50f, 50f - font.fontHeight - 1, 0xFFFFFFFF);
+            for (int i = 0; i < tooltipText.size(); i++) {
+                Component component = tooltipText.get(i);
+                font.drawWithShadow(new TranslatableComponent(component.getText()).getText(), 50f, 50f + (i * font.fontHeight + 1), 0xFFFFFFFF);
+            }
         }
     }
 }
--- src/main/java/io/github/blayyke/fabrictoys/config/ConfigVals.java
+++ src/main/java/io/github/blayyke/fabrictoys/config/ConfigVals.java
@@ -1,14 +1,10 @@
 package io.github.blayyke.fabrictoys.config;
 
 import blue.endless.jankson.Comment;
-import net.minecraft.util.math.Vec3d;
 
 public class ConfigVals {
     @Comment("Tweak that makes placed campfires not be lit by default.")
     public boolean enableCampfireTweak = true;
-
-    @Comment("Tweak that allows harvesting crops when right clicked.")
-    public boolean enableCropHarvestTweak = true;
 
     @Comment("The timeout that players are kicked after when a keepalive has not been received in the time period.\nThe default for vanilla is 15000.")
     public long keepaliveTimeout = 50000L;
@@ -18,4 +14,7 @@ public class ConfigVals {
 
     @Comment("Allow placing eggs on the ground for a 100% chicken spawn rate.")
     public boolean enableEggPlacement = true;
+
+    @Comment("Show information about the item stored in the item frame you are looking at. (client only)")
+    public boolean enableItemFrameInfo = true;
 }
